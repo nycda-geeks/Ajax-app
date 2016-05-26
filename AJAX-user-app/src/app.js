f05dc838fa17697dddea5048c503dda367c96bff
Anthony <anthony_wever@yahoo.com>
2016-05-26 16:50:45 +0200

    fixed issue with search bar

--- AJAX-user-app/src/app.js
+++ AJAX-user-app/src/app.js
@@ -49,7 +49,7 @@ app.get('/search', function (req, res) {
 app.post('/search', function (req, res) { 
   giveData( function(file) {
     var allusers =[];
-    console.log(req.body)
+    console.log(req.body.name)
 
     file.forEach(function (user){
       if ( req.body.name === user.firstname || req.body.email === user.email ) 
@@ -58,13 +58,41 @@ app.post('/search', function (req, res) {
     res.send(allusers)
   })
 });
-
+app.get('/users/userfound', function (req, res) {
+  res.render('userfound',{results: "bloop"})
+});
 app.get('/newuser', function (req, res) {
   res.render('new-user')
 });
 
 app.get('/users/search', function(request, response) {
   response.render('search');
+});
+
+app.post('/users/userfound', bodyParser.urlencoded({
+  extended: true
+}), function(request, response) {
+  fs.readFile('./resources/users.json', function(err, data) {
+    if (err) {
+      throw err;
+    }
+    console.log("bloops")
+    users = JSON.parse(data);
+    var results = [];
+    
+    for (var i = 0; i < users.length; i++) {
+
+      console.log(request.body)
+      if (users[i].firstname.indexOf(request.body.choices) != -1 || users[i].lastname.indexOf(request.body.choices) != -1) {
+    
+        results.push(users[i].firstname + " " + users[i].lastname)
+
+
+      }
+    };
+    console.log(results)
+    response.render('userfound', {results:results})
+  });
 });
 
 
@@ -104,9 +132,9 @@ app.post('/users/search', bodyParser.urlencoded({
     }
     users = JSON.parse(data);
     var results = [];
-    console.log('Search term: ' + request.body.autocomplete)
+    console.log("setp 1")
     for (i = 0; i < users.length; i++) {
-      console.log('Comparing ' + i + ' user')
+      console.log('step 2')
       console.log('Index returned: ' + users[i].firstname.indexOf(request.body.autocomplete) )
       if ((users[i].firstname.indexOf(request.body.autocomplete) != -1) || (users[i].lastname.indexOf(request.body.autocomplete) != -1)) {
         console.log('if triggered')
@@ -124,15 +152,19 @@ app.get('/users/searchresult', function(request, response) {
     }
     users = JSON.parse(data);
     var results = [];
+    console.log('banana')
+    console.log(request.query.firstname);
 
     for (i = 0; i < users.length; i++) {
       if (users[i].firstName === request.query.firstname || users[i].lastname === request.query.lastname) {
         results = results.concat(users[i]);
       }
     }
-    response.render('searchresult', {
+    response.render('userfound', {
       results: results
     });
   });
 });
+
+
 app.listen(3000)
--- AJAX-user-app/src/views/autocomplete.js
+++ AJAX-user-app/src/views/autocomplete.js
@@ -2,20 +2,33 @@ var time = 0;
 var lasttime = 0;
 
 
-$(document).ready(function() {	
+$(document).ready(function() {
+	var colours = ["#000000", "#FF0000", "#990066", "#FF9966", "#996666", "#00FF00", "#CC9933"], 
+	idx;
+
+	$(function() {
+		var div = $('h1'); 
+		var chars = div.text().split('');
+		div.html('');     
+		for(var i=0; i<chars.length; i++) {
+			idx = Math.floor(Math.random() * colours.length);
+			var span = $('<span>' + chars[i] + '</span>').css("color", colours[idx]);
+			div.append(span);
+		}
+	});	
 	var cleartime;
-$("#autocomplete").on('keyup', function() {
-	
+	$("#autocomplete").on('keyup', function() {
+
 		console.log("bloop")
 		// time = Date.now()
 		// if (time - lasttime > 300 || time === 0) {
 		// 	console.log("Updating" + (time-lasttime))
 		// 	lasttime = time;
 
-			var postdata = {
-				autocomplete: $("#autocomplete").val()
-			}
-			cleartime = setTimeout(function() {
+		var postdata = {
+			autocomplete: $("#autocomplete").val()
+		}
+		cleartime = setTimeout(function() {
 
 			$.post("/users/search/", postdata, function(results) {
 				console.log(results)
@@ -35,6 +48,8 @@ $("#autocomplete").on('keyup', function() {
 			
 		})
 		// }
-}, 300);
+	}, 300);
+	});
 });
-});
+
+
